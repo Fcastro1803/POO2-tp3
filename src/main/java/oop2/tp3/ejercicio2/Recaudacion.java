be21aa91f839c0cb25e4b7d5373756367844592a
Fcastro1803 <federicocastro.18.94@gmail.com>
2026-04-24 10:28:20 -0300

    -Se transformo el metodo estatico en uno de instancia.  -Se modifico test para crear una instancia de Recaudacion para poder llamar al metodo where. -Se removio el codigo duplicado de los foreach creando metodos privados.

--- src/main/java/oop2/tp3/ejercicio2/Recaudacion.java
+++ src/main/java/oop2/tp3/ejercicio2/Recaudacion.java
@@ -7,70 +7,49 @@ import java.util.List;
 import java.util.Map;
 
 public class Recaudacion {
-    public static List<Map<String, String>> where(Map<String, String> options)
-            throws IOException {
-        List<String[]> csvData = Csv.readCsv();
+    private List<String[]> csvData;
 
-        if (options.containsKey("company_name")) {
-            List<String[]> results = new ArrayList<String[]>();
+    public Recaudacion() throws IOException {
+        this.csvData = Csv.readCsv();
+    }
 
-            for (String[] fila : csvData) {
-                if (fila[1].equals(options.get("company_name"))) {
-                    results.add(fila);
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("city")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] fila : csvData) {
-                if (fila[4].equals(options.get("city"))) {
-                    results.add(fila);
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("state")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] fila : csvData) {
-                if (fila[5].equals(options.get("state"))) {
-                    results.add(fila);
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("round")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] fila : csvData) {
-                if (fila[9].equals(options.get("round"))) {
-                    results.add(fila);
-                }
-            }
-            csvData = results;
-        }
-
+    public List<Map<String, String>> where(Map<String, String> options) {
+        filtrarPor(options, "company_name", 1);
+        filtrarPor(options, "city", 4);
+        filtrarPor(options, "state", 5);
+        filtrarPor(options, "round", 9);
         List<Map<String, String>> output = new ArrayList<Map<String, String>>();
-
         for (String[] fila : csvData) {
-            Map<String, String> mapped = new HashMap<String, String>();
-            mapped.put("permalink", fila[0]);
-            mapped.put("company_name", fila[1]);
-            mapped.put("number_employees", fila[2]);
-            mapped.put("category", fila[3]);
-            mapped.put("city", fila[4]);
-            mapped.put("state", fila[5]);
-            mapped.put("funded_date", fila[6]);
-            mapped.put("raised_amount", fila[7]);
-            mapped.put("raised_currency", fila[8]);
-            mapped.put("round", fila[9]);
-            output.add(mapped);
+            output.add(mapearFila(fila));
         }
         return output;
+    }
+
+    private static Map<String, String> mapearFila(String[] fila) {
+        Map<String, String> mapped = new HashMap<String, String>();
+        mapped.put("permalink", fila[0]);
+        mapped.put("company_name", fila[1]);
+        mapped.put("number_employees", fila[2]);
+        mapped.put("category", fila[3]);
+        mapped.put("city", fila[4]);
+        mapped.put("state", fila[5]);
+        mapped.put("funded_date", fila[6]);
+        mapped.put("raised_amount", fila[7]);
+        mapped.put("raised_currency", fila[8]);
+        mapped.put("round", fila[9]);
+        return mapped;
+    }
+
+    private void filtrarPor(Map<String, String> options, String company_name, int x) {
+        if (options.containsKey(company_name)) {
+            List<String[]> results = new ArrayList<String[]>();
+
+            for (String[] fila : csvData) {
+                if (fila[x].equals(options.get(company_name))) {
+                    results.add(fila);
+                }
+            }
+            csvData = results;
+        }
     }
 }
